fa9e96bff2d02d5b56dbc9713884045af3808f60
Abdullah-Alsalhi <abuhamed19@gmail.com>
2021-11-10 03:06:31 +0300

    computation

--- README.md
+++ README.md
@@ -5,8 +5,17 @@
 
 ###### Try these opreations on console:
 
-- let result_6 = 9 + 2;
-- let result_7 = 1 - 3;
-- let result_8 = 2 * 3;
-- let result_9 = 6 / 2;
-- let result_10 = 15 % 4;
+     let result_6 = 9 + 2;
+     let result_7 = 1 - 3;
+     let result_8 = 2 * 3;
+     let result_9 = 6 / 2;
+     let result_10 = 15 % 4;
+
+###### Try these compute opreations:
+
+     let result_16 = 9 - 2 + 8;
+     let result_17 = 1 - 3 + 6;
+     let result_18 = 2 * (3 + 9);
+     let result_19 = 6 / 2 * (1 - 5);
+     let result_20 = 15 % 2 - 3 * 3;
+     let result_21 = 5 % 2 + 10 / 5;
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-console.log('Week 1 - Day 3')
-console.log('Tuwaiq Academy   JS')
+console.log('Week 1 - Day 3');
+console.log('Tuwaiq Academy   JS');
 
 // Exercise - Arthemetic Opreations:
 
@@ -9,8 +9,24 @@ let result_8 = 2 * 3;
 let result_9 = 6 / 2;
 let result_10 = 15 % 4;
 
-console.log(result_6)
-console.log(result_7)
-console.log(result_8)
-console.log(result_9)
-console.log(result_10)
+console.log(result_6);
+console.log(result_7);
+console.log(result_8);
+console.log(result_9);
+console.log(result_10);
+
+// Exercise - Computation 
+
+let result_16 = 9 - 2 + 8;
+let result_17 = 1 - 3 + 6;
+let result_18 = 2 * (3 + 9);
+let result_19 = 6 / 2 * (1 - 5);
+let result_20 = 15 % 2 - 3 * 3;
+let result_21 = 5 % 2 + 10 / 5;
+
+console.log(result_16);
+console.log(result_17);
+console.log(result_18);
+console.log(result_19);
+console.log(result_20);
+console.log(result_21);
