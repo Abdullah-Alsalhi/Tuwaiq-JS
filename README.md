a0fb83502f2d074c82cad325c2aa3ce59132ac35
Abdullah-Alsalhi <abuhamed19@gmail.com>
2021-11-11 01:57:49 +0300

    Exercise - Logic Opreations

--- README.md
+++ README.md
@@ -1,50 +1,17 @@
 # Tuwaiq-JS
 **Tuwaiq-Academey course of JavaScript programming language.**
 
-### Exercise - Arthemetic Operations
+### Exercise - logic Operations (AND(&&) - OR(||) - NOT(!))
 
-###### Try these opreations on console:
+###### Try these logic opreations on console:
 
-     let result_6 = 9 + 2;
-     let result_7 = 1 - 3;
-     let result_8 = 2 * 3;
-     let result_9 = 6 / 2;
-     let result_10 = 15 % 4;
-
-### Exercise - Computation
-
-###### Try these compute opreations:
-
-     let result_16 = 9 - 2 + 8;
-     let result_17 = 1 - 3 + 6;
-     let result_18 = 2 * (3 + 9);
-     let result_19 = 6 / 2 * (1 - 5);
-     let result_20 = 15 % 2 - 3 * 3;
-     let result_21 = 5 % 2 + 10 / 5;
-
-### Exercise - Comparasions
-
-###### Try these comapre opreations:
-
-     let result_1b = 3 == 7;
-     let result_2b = 10 != 8;
-     let result_3b = 3 > 5;
-     let result_4b = 4 >= 3;
-     let result_5b = 4 < 55;
-     let result_6b = 6 <= 3;
-     let result_7b = 7 === 7;
-     let result_8b = 7 !== 7;
-
-## Homework - Day-3
-
-     let hw_D03_1 = 1 - 3 + 6 > 1 - 8;
-     let hw_D03_2 = 2 * (3 + 9) >= 9 - 1 * 0;
-     let hw_D03_3 = (6/2) * 5 <= 1 - 5;
-     let hw_D03_4 = 15 % 2 > 9 - 1;
-     let hw_D03_5 = 7 * 7 === '4' + '9';
-     let hw_D03_6 = 7 * 7 == '4' + '9';
-     let hw_D03_7 = 7 * 7 !== '4' + '9';
-     let hw_D03_8 = 7 * 7 != '4' + '9';
-     let hw_D03_9 = 7 > '7';
-     let hw_D03_10 = 7 <= '7';
-     let hw_D03_11 = 9 + 2 > 8 - 5 * 9;
+     let ex_1b = (false && false) || true;
+     let ex_2b = (false && true) || false;
+     let ex_3b = 10 > 25 * 12 || false;
+     let ex_4b = true || (17 * 1 > 4 && true);
+     let ex_5b = (!false && true) || !false;
+     let ex_6b = 3 * 7 + 4 <= 6 || (true && 7 > 8);
+     let ex_7b = (true && (true || false)) || false;
+     let ex_8b = (false && true) || true || false;
+     let ex_9b = (15 % 3) + 6 > 10 || 40 < 7;
+     let ex_10b = 9 + 6 > 10 && 13 <= 33;
--- main.js
+++ main.js
@@ -1,84 +1,15 @@
-console.log('Week 1 - Day 3');
+console.log('Week 1 - Day 4');
 console.log('Tuwaiq Academy   JS');
 
-// Exercise - Arthemetic Opreations:
+// Exercise - Logic opreations
 
-let result_6 = 9 + 2;
-let result_7 = 1 - 3;
-let result_8 = 2 * 3;
-let result_9 = 6 / 2;
-let result_10 = 15 % 4;
-
-console.log(result_6);
-console.log(result_7);
-console.log(result_8);
-console.log(result_9);
-console.log(result_10);
-
-console.log('----------')
-
-// Exercise - Computation 
-
-let result_16 = 9 - 2 + 8;
-let result_17 = 1 - 3 + 6;
-let result_18 = 2 * (3 + 9);
-let result_19 = 6 / 2 * (1 - 5);
-let result_20 = 15 % 2 - 3 * 3;
-let result_21 = 5 % 2 + 10 / 5;
-
-console.log(result_16);
-console.log(result_17);
-console.log(result_18);
-console.log(result_19);
-console.log(result_20);
-console.log(result_21);
-
-console.log('----------')
-
-// Exersice - Comparasions
-
-let result_1b = 3 == 7;
-let result_2b = 10 != 8;
-let result_3b = 3 > 5;
-let result_4b = 4 >= 3;
-let result_5b = 4 < 55;
-let result_6b = 6 <= 3;
-let result_7b = 7 === 7;
-let result_8b = 7 !== 7;
-
-console.log(result_1b);
-console.log(result_2b);
-console.log(result_3b);
-console.log(result_4b);
-console.log(result_5b);
-console.log(result_6b);
-console.log(result_7b);
-console.log(result_8b);
-
-console.log('----------')
-
-// Homework - Day-3
-
-let hw_D03_1 = 1 - 3 + 6 > 1 - 8;
-let hw_D03_2 = 2 * (3 + 9) >= 9 - 1 * 0;
-let hw_D03_3 = (6/2) * 5 <= 1 - 5;
-let hw_D03_4 = 15 % 2 > 9 - 1;
-let hw_D03_5 = 7 * 7 === '4' + '9';
-let hw_D03_6 = 7 * 7 == '4' + '9';
-let hw_D03_7 = 7 * 7 !== '4' + '9';
-let hw_D03_8 = 7 * 7 != '4' + '9';
-let hw_D03_9 = 7 > '7';
-let hw_D03_10 = 7 <= '7';
-let hw_D03_11 = 9 + 2 > 8 - 5 * 9;
-
-console.log(hw_D03_1);
-console.log(hw_D03_2);
-console.log(hw_D03_3);
-console.log(hw_D03_4);
-console.log(hw_D03_5);
-console.log(hw_D03_6);
-console.log(hw_D03_7);
-console.log(hw_D03_8);
-console.log(hw_D03_9);
-console.log(hw_D03_10);
-console.log(hw_D03_11);
+let ex_1b = (false && false) || true; // true
+let ex_2b = (false && true) || false; // false
+let ex_3b = 10 > 25 * 12 || false; // false
+let ex_4b = true || (17 * 1 > 4 && true); // true
+let ex_5b = (!false && true) || !false; // true
+let ex_6b = 3 * 7 + 4 <= 6 || (true && 7 > 8); // false
+let ex_7b = (true && (true || false)) || false; // true
+let ex_8b = (false && true) || true || false; // true
+let ex_9b = (15 % 3) + 6 > 10 || 40 < 7; // false
+let ex_10b = 9 + 6 > 10 && 13 <= 33; // true
