8595ec674d1f19ebceabe77154cc430bbbced712
Abdullah-Alsalhi <abuhamed19@gmail.com>
2021-11-18 03:16:47 +0300

    Exercise-1

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Tuwaiq-JS
 **Tuwaiq-Academey course of JavaScript programming language.**
 
-#### Exercise - 
+#### Exercise - for loop
+
+**using following array:**
+```javascript
+let exer_1 = ["Raghad", "Khalid", "Suhaib", "Ahmad"];
+```
+1. create a function takes an array and print its items.
+1. create a function takes an array and print greeting message to everyone in the array by their name.
--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 console.log('Week 2 - Day 3');
 console.log('Tuwaiq Academy   JS');
 
+// create a function takes an array and print its items.
+
+let exer_1 = ["Raghad", "Khalid", "Suhaib", "Ahmad"];
+
+function ArrayPrint(array){
+    for (let i = 0; i < array.length; i++){
+        let output = array[i];
+        console.log(output);
+    };
+};
+
+ArrayPrint(exer_1);
+
+
+// create a function takes an array and print greeting message to everyone in the array by their name.
+
+function greetStudent(students){
+    for (let i = 0; i < students.length; i++){
+        output = students[i];
+        message = "Welcome " + output;
+        console.log(message);
+    };
+};
+
+greetStudent(exer_1);
