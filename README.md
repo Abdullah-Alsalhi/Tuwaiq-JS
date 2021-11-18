238e991877f729aeac319fb6603b0b7937d83379
Abdullah-Alsalhi <abuhamed19@gmail.com>
2021-11-18 03:57:42 +0300

    exercise 2

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ let exer_1 = ["Raghad", "Khalid", "Suhaib", "Ahmad"];
 ```
 1. create a function takes an array and print its items.
 1. create a function takes an array and print greeting message to everyone in the array by their name.
+
+**using following array:**
+```javascript
+let exer_2 = ["Raghad", "Khalid", "Suhaib", "Ahmad"];
+```
+
+1. create a function takes an array containing string items and print the length of each item.
+1. create a function takes an array containing string and change the items to be " Welcome $item ".
+1. create a function takes an array and print only the numbers dividable over 3. 
+```javascript
+let exer_3 = [17, 15, 8, 12, 7, 25, 24];
+```
--- main.js
+++ main.js
@@ -26,3 +26,41 @@ function greetStudent(students){
 };
 
 greetStudent(exer_1);
+
+// create a function takes an array containing string items and print the length of each item.
+
+let exer_2 = ["Raghad", "Khalid", "Suhaib", "Ahmad"];
+
+function itemLength(array){
+    for (let i = 0; i < array.length; i++){
+        output = array[i].length;
+        console.log(output);
+    };
+};
+
+itemLength(exer_2);
+
+// create a function takes an array containing string and change the items to be " Welcome $item ".
+
+function welcoming(array){
+    for (let i = 0; i < array.length; i++){
+        array[i] = "Welcome " + array[i];
+        // console.log(array[i]);
+    };
+};
+
+welcoming(exer_2);
+
+// create a function takes an array and print only the numbers dividable over 3. 
+
+function module3(array){
+    for (let i = 0; i < array.length; i++){
+        if (array[i] % 3 === 0){
+            console.log(array[i])
+        }
+    };
+};
+
+let exer_3 = [17, 15, 8, 12, 7, 25, 24];
+
+module3(exer_3);
