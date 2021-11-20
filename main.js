e9684f22cf11cc8b68ba7d36a58794aaa7f587e3
Abdullah-Alsalhi <abuhamed19@gmail.com>
2021-11-20 07:41:18 +0300

    add: exercises

--- main.js
+++ main.js
@@ -28,3 +28,56 @@ information = {
 information["fName"];
 information["favoriteFood"];
 information["profession"];
+
+// Exercise - Your first name , last name, your age , your favorite food, your profession and your salary.
+// create a function inside an object to increase the salary 100 SR and return the final value.
+// create a function inside the object to change first name through a name a given name to this function.
+
+infoAboutMe = {
+    fNmae: "Abdullah",
+    lName: "Alsalhi",
+    age: 27,
+    favoriteFood: "Broasted",
+    profession: "DevOps engineer",
+    salary: 15000,
+    salaryIncrease: function(){
+        this.salary = this.salary + 100;
+        return this.salary;
+    },
+    changeFname: function(name){
+        this.fName = name
+        return "The name has been changed, to check run <infoAboutMe.fName>";
+    }
+};
+
+infoAboutMe.salaryIncrease(); // call a function inside an object
+
+infoAboutMe.changeFname("Ali"); // call a function inside an object and insert an argument to it
+
+// Exercise - Your first name , last name, your age , your favorite food and your profession.
+
+details = {
+    fNmae: "Mohammed",
+    lName: "Jouza",
+    age: 27,
+    favoriteFood: "fruits",
+    profession: "Lead instructor"
+};
+
+// print each key using for in. [point 1]
+
+for (let key in details){
+    console.log(key, ":" ,details[key]);
+};
+
+// create an object with : car type, prize, traveling in KM, color.
+
+car = {
+    type: "Mercedes",
+    prize: 3766788,
+    km: 150
+};
+
+for (let value in car){
+    console.log(value, ":", car[value]);
+};
