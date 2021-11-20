cba1fecf59bbe989690661093a99bcd1f8c04829
Abdullah-Alsalhi <abuhamed19@gmail.com>
2021-11-20 04:50:58 +0300

    add: Exercise one

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Tuwaiq-JS
 **Tuwaiq-Academey course of JavaScript programming language.**
 
-#### Exercise - for loop
-
+#### Exercise - Object / Dictionary
+**create the following objects:**
+1. Your first name , last name, your age , your favorite food and your profession.
+1. print each one using bracket.
+1. *repeat number 1* on someone else (without last name, and age).
+1. *repeat number 2* and notice the difference.
--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 console.log('Week 2 - Day 4');
 console.log('Tuwaiq Academy   JS');
 
+// Exercise - Your first name , last name, your age , your favorite food and your profession.
+
+myInfo = {
+    fName: "Abdullah",
+    lName: "Alsalhi",
+    age: 27,
+    favoriteFood: "Pizza",
+    profession: "DevOps Engineer"
+};
+
+myInfo["fName"];
+myInfo["lName"];
+myInfo["age"];
+myInfo["favoriteFood"];
+myInfo["profession"];
+
+// repeat number 1 on someone else (without last name, and age).
+
+information = {
+    fName:"Ali",
+    favoriteFood: "Rice",
+    profession: "Electrical Engineer"
+};
+
+information["fName"];
+information["favoriteFood"];
+information["profession"];
