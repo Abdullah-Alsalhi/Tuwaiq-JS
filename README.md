46c94387e2c14278d245f2ccbe7e0d2f130449e5
Abdullah-Alsalhi <abuhamed19@gmail.com>
2021-11-21 23:14:23 +0300

    add: exercises

--- README.md
+++ README.md
@@ -8,3 +8,11 @@
 1. function takes an array of numbers and print each number multiplied into 2.
 1. function takes an array consisting of string and return new array consisting of string length bigger than 4.
 1. function takes an array of numbers and return only numbers bigger than 3.
+
+- **create the following functions and call them**
+1. function takes an array of numbers and return new array based on the array multiply 2.
+1. function takes an array of numbers and return new array based on the array[item] + 4.
+1. function takes an array of numbers and return first item dividable by 5.
+1. function takes an array of string and return first item has length of 4.
+
+## Homework
--- main.js
+++ main.js
@@ -14,7 +14,7 @@ printAll(fruits);
 
 // function takes an array of numbers and print each number multiplied into 2.
 
-numbers = [1, 2, 3, 14, 21, 28];
+numbers = [1, 2, 3, 10, 14, 21, 28];
 
 function printMult2(array){
     numbers.forEach( (num, index) =>{
@@ -43,3 +43,47 @@ function retNum(array){
 };
 
 retNum(numbers);
+
+// Exercise - function takes an array of numbers and return new array based on the array multiply 2.
+
+function new_array(array){
+    let output = array.map((item, index)=>{
+        return item * 2;
+    });
+    return output;
+};
+
+new_array(numbers);
+
+// function takes an array of numbers and return new array based on the array[item] + 4.
+
+function arr(array){
+    let output = array.map((item, index)=>{
+        return item + 4;
+    })
+    return output;
+};
+
+arr(numbers);
+
+// function takes an array of numbers and return first item dividable by 5.
+
+function dividable5(array){
+    let output = array.find((item, index)=>{
+        return item % 5 === 0;
+    })
+    return output;
+};
+
+dividable5(numbers);
+
+// function takes an array of string and return first item has length of 4.
+
+function lengthArray(array){
+    let output = array.find((item, index)=>{
+        return item.length = 4;
+    })
+    return output;
+};
+
+lengthArray(fruits);
