cda0df715780f8aa070e0b4f16420d430d7ccd97
Abdullah-Alsalhi <abuhamed19@gmail.com>
2021-11-21 21:32:32 +0300

    add: Exercises

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Tuwaiq-JS
 **Tuwaiq-Academey course of JavaScript programming language.**
 
+**JavaScript Advanced level**:
+
+- **create the following functions and call them**
+1. function takes an array consisting of string and print each item in it.
+1. function takes an array of numbers and print each number multiplied into 2.
+1. function takes an array consisting of string and return new array consisting of string length bigger than 4.
+1. function takes an array of numbers and return only numbers bigger than 3.
--- main.js
+++ main.js
@@ -1,3 +1,45 @@
 console.log('Week 3 - Day 1');
 console.log('Tuwaiq Academy   JS');
 
+// Exercise - function takes an array consisting of string and print each item in it.
+
+fruits = ["Apple", "Orange", "Banana", "Strawberry", "Fig"];
+
+function printAll(array){
+    fruits.forEach( (item, index) =>{
+        console.log(item);
+})};
+
+printAll(fruits);
+
+// function takes an array of numbers and print each number multiplied into 2.
+
+numbers = [1, 2, 3, 14, 21, 28];
+
+function printMult2(array){
+    numbers.forEach( (num, index) =>{
+        console.log(num * 2);
+})};
+
+printMult2(numbers);
+// function takes an array consisting of string and return new array consisting of string length bigger than 4.
+
+function nArray(array){
+    let output = fruits.filter((item, index) =>{
+        return item.length > 4;
+})
+    return output;
+};
+
+nArray(fruits);
+
+// function takes an array of numbers and return only numbers bigger than 3.
+
+function retNum(array){
+    let output = numbers.filter((num, index)=>{
+        return num > 3;
+})
+    return output;
+};
+
+retNum(numbers);
