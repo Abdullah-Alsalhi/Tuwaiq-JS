03c8b74fd65003aa707747fcefe9445037ccb7d2
Abdullah-Alsalhi <abuhamed19@gmail.com>
2021-12-09 19:37:33 +0300

    add: files

--- app.js
+++ app.js
@@ -1,33 +1,63 @@
-const express = require('express');
-const db = require('./db');
-const User = require('./models/user');
+const express = require("express");
+const db = require("./db");
+const User = require("./models/user");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post('/insert', (req, res)=>{
-  User.create(req.body, (err)=>{
-    if (err){
+app.post("/insert", (req, res) => {
+  User.create(req.body, (err) => {
+    if (err) {
       return handleError(err);
-    }else{
+    } else {
       res.json("CREATED NEW USER SUCCESSFULLY");
-    };
+    }
   });
 });
 
-app.get('/', (req, res)=>{
-  User.find({}, (err, data)=>{
-    if (err){
+app.get("/", (req, res) => {
+  User.find({}, (err, data) => {
+    if (err) {
       return handleError(err);
-    }else{
-      res.json(data)
-    };
+    } else {
+      res.json(data);
+    }
   });
 });
 
+app.delete("/users/:user", (req, res) => {
+  User.deleteOne({ favFood: req.params.user }, (err, deletedObj) => {
+    if (err) {
+      return handleError(err);
+    } else {
+      if (deletedObj.deletedCount === 0) {
+        res.status(404).json(req.params.user + " not found");
+      } else {
+        res.status(200).json("Deleted last user successfully");
+      }
+    }
+  });
+});
 
+app.put("/edit/:oldFood", (req, res) => {
+  User.updateOne(
+    { favFood: req.params.oldFood },
+    { favFood: req.body.newFavFood },
+    (err, updatedObj) => {
+      if (err) {
+        return handleError(err);
+      } else {
+        if (updatedObj.matchedCount === 0) {
+          res.status(404).json(req.params.oldFood + "not found");
+        } else {
+          res.status(200).json(req.body.newFavFood + " Update successfully");
+        }
+      }
+    }
+  );
+});
 
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log("SERVER IS RUNNING NORMALLY ON", port);
 });
--- db.js
+++ db.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/test';
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost:27017/test";
 const db = mongoose.connection;
 
-mongoose.connect(mongoURI,{}, ()=>{
-  console.log("connection is established")
+mongoose.connect(mongoURI, {}, () => {
+  console.log("connection is established");
 });
 
-db.on('Error', (err)=>{
-  console.log(err.message,"mongoose not working" );
+db.on("Error", (err) => {
+  console.log(err.message, "mongoose not working");
 });
 
-db.on('connected', ()=>{
-  console.log('DATABASE CONNECTED');
+db.on("connected", () => {
+  console.log("DATABASE CONNECTED");
 });
